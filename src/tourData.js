c43735124063274706f759f1bcf1b179a2484b5c
Sid Narla <sridhar.narla@thameswater.co.uk>
2019-05-08 12:49:12 +0100

    first commit

---
+++ src/tourData.js
@@ -0,0 +1,35 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "Paphos",
+    img: "./img/PephosTeam.jpg",
+    name: "Akamas Peninsula Boat Trip",
+    info:
+      "The western tip of Cyprus is an area known as the Akamas Peninsula, and it is one of the least-inhabited parts of the island. If you’re eager to get off the beaten track, then this is the place to be: There are literally no paved roads! As a result, it is ideal for hiking or mountain biking. It is also an amazing place for spotting wildlife, just some of which can include sea turtles, reptiles and native birds. You can also explore the history of the Akamas Peninsula with a visit to the Agios Andronikos, a former mosque and current church that was constructed in the early 16th century."
+      
+  },
+  {
+    id: 2,
+    city: "Amersham",
+    img: "./img/Amrsham.jpg",
+    name: "Amersham tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  },
+  {
+    id: 3,
+    city: "london",
+    img: "./img/london.jpeg",
+    name: "london royal palace tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  },
+  {
+    id: 4,
+    city: "tokyo",
+    img: "./img/tokyo.jpeg",
+    name: "tokyo sushi tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  }
+];
